1fcfe638ed5755d4160358ef7c6c41d6bedd443f
Hajime Todo <samsepylot@gmail.com>
2022-09-26 22:54:50 +0900

    こっちでもいける

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 
 const createCache = () => {
-  const cache: Record<string, string> = {};
+  const cache: { [key: string]: string } = {};
 
   const add = (id: string, value: string) => {
     cache[id] = value;
