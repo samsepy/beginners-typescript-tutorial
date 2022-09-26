d5ce59b4ca03ea8d2842482ae05ad73ada47e376
Hajime Todo <samsepylot@gmail.com>
2022-09-27 00:34:47 +0900

    ?記法でもマージ可

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -10,15 +10,11 @@ interface Post {
   body: string;
 }
 
-interface Merge extends User {
-  posts: Post[];
-}
-
 /**
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): Merge  => {
+export const getDefaultUserAndPosts = (): User & { posts: Post[] }  => {
   return {
     id: "1",
     firstName: "Matt",
